c0ee62082585c9f6ee92ad1d8a9d613c35ccfb82
Monster2408 <meoto2408@gmail.com>
2024-04-25 06:41:07 +0900

    up

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MLServerModChecker
+## About
 Forge Mod for Minecraft 1.12.2-14.23.5.2859
+
+## How to use
+1. login to the local world
+2. use the command `/modcheck` to check the server mods
+3. use the command `/modcheck confirm` to check the server mods
--- src/main/java/xyz/mlserver/mod/mlservermodchecker/commands/ModCheckCmd.java
+++ src/main/java/xyz/mlserver/mod/mlservermodchecker/commands/ModCheckCmd.java
@@ -11,10 +11,18 @@ import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.event.ClickEvent;
 import net.minecraft.util.text.event.HoverEvent;
 import net.minecraftforge.fml.common.ModContainer;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import xyz.mlserver.mod.mlservermodchecker.MLServerModChecker;
 import xyz.mlserver.mod.mlservermodchecker.MLServerModCheckerWhitelistMods;
 
+import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ModCheckCmd extends CommandBase {
 
@@ -59,19 +67,11 @@ public class ModCheckCmd extends CommandBase {
             Style style;
             for (ModContainer mod : list) {
                 comp = new TextComponentString("- " + mod.getName() + "(" + mod.getModId() + ") - バージョン: " + mod.getVersion());
-//                style = new Style().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, mod.getUpdateUrl()) {
-//                    @Override
-//                    public Action getAction() {
-//                        //custom behavior
-//                        return Action.OPEN_URL;
-//                    }
-//                });
-//                comp.setStyle(style);
                 player.sendMessage(comp);
             }
             comp = new TextComponentString("");
             player.sendMessage(comp);
-            String message = "ホワイトリストに登録されていないModがあります。クリックでレポートを送信可能です。(強制するものではありませんがMLServerでは禁止されています。)";
+            String message = "ホワイトリストに登録されていないModがあります。クリックで許可申請を送信可能です。(強制するものではありませんがMLServerでは禁止されています。)";
             comp = new TextComponentString(message);
             style = new Style().setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/modcheck sendrequest") {
                 @Override
@@ -84,7 +84,64 @@ public class ModCheckCmd extends CommandBase {
             // 3s後にメッセージを表示
             player.sendMessage(comp);
         } else if (args[0].equalsIgnoreCase("sendrequest")) {
+            List<ModContainer> list = MLServerModCheckerWhitelistMods.getNoWhitelistMods();
+            if (list.isEmpty()) {
+                String message = "ホワイトリストに登録されていないModはありません。";
+                TextComponentString comp = new TextComponentString(message);
+                player.sendMessage(comp);
+                return;
+            }
+            String message = "{" +
+                    "    \"data\": [";
+            for (ModContainer mod : list) {
+                message +=
+                        "        {" +
+                        "            \"modId\": \"" + mod.getModId() + "\"," +
+                        "            \"modName\": \"" + mod.getName() + "\"," +
+                        "            \"modVersion\": \"" + mod.getVersion() + "\"" +
+                        "        },";
+            }
+            message += "    ]" +
+                    "}";
+            try {
+                String result = callWebAPI(message);
+                System.out.println(result);
+                TextComponentString comp = new TextComponentString("許可申請を送信しました。");
+                player.sendMessage(comp);
+            } catch (IOException e) {
+                e.printStackTrace();
+                TextComponentString comp = new TextComponentString("許可申請の送信に失敗しました。");
+                player.sendMessage(comp);
+            }
 
         }
+    }
+
+    private static final String WEB_API_ENDPOINT = "https://wiki.mlserver.xyz/api/whitelist_mods/request.php";
+
+    public String callWebAPI(String postJson) throws IOException {
+
+        final Map<String, String> httpHeaders = new LinkedHashMap<String, String>();
+        final String resultStr = doPost(WEB_API_ENDPOINT, "UTF-8", httpHeaders, postJson);
+
+        return resultStr;
+    }
+
+    public String doPost(String url, String encoding, Map<String, String> headers, String jsonString) throws IOException {
+        final okhttp3.MediaType mediaTypeJson = okhttp3.MediaType.parse("application/json; charset=" + encoding);
+
+        final RequestBody requestBody = RequestBody.create(mediaTypeJson, jsonString);
+
+        final Request request = new Request.Builder()
+                .url(url)
+                .headers(Headers.of(headers))
+                .post(requestBody)
+                .build();
+
+        final OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+        final Response response = client.newCall(request).execute();
+        final String resultStr = response.body().string();
+        return resultStr;
     }
 }
